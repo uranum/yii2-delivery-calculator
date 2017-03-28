afd107a11291774a53137d46b9b698eca3408b6a
Eugeny_Emelyanov <e9139905539@gmail.com>
2017-03-29 01:30:43 +0700

    EnergyDelivery

--- src/services/EnergyDelivery.php
+++ src/services/EnergyDelivery.php
@@ -9,12 +9,22 @@ namespace uranum\delivery\services;
 
 use uranum\delivery\services\calculators\EnergyCalc;
 use uranum\delivery\services\calculators\PlaceParams;
+use yii\helpers\VarDumper;
 
 class EnergyDelivery extends YiiModuleDelivery
 {
+	private $transfers = [];
+	
+	public function getResult()
+	{
+		if ($this->validate()) {
+			return $this->transfers;
+		} else {
+			return false;
+		}
+	}
 	
 	/**
-	 *
 	 * @return mixed
 	 */
 	public function calculate()
@@ -27,5 +37,31 @@ class EnergyDelivery extends YiiModuleDelivery
 		);
 		
 		$calculator = new EnergyCalc($this->zip, $this->moduleParams, $place->toArray());
+		$result     = $calculator->calculate();
+		$this->handleResult($result);
+		//VarDumper::dump($result, 10, true);
+		//die();
+	}
+	
+	private function handleResult($result)
+	{
+		$this->info = $this->serviceParams->info . ' ';
+		$this->name = $this->serviceParams->name;
+		
+		if (array_key_exists('transfer', $result)) {
+			foreach ($result['transfer'] as $type) {
+				$this->transfers[] = [
+					'name'  => $this->serviceParams->name . '-' . $type['type'],
+					'terms' => $type['interval'],
+					'cost'  => $type['price'],
+					'info'  => $this->serviceParams->info . ' ',
+				];
+			}
+		} else {
+			if (is_array($result['error'])) {
+				$this->transfers[]['info'] = $result['error']['message'];
+				
+			}
+		}
 	}
 }
