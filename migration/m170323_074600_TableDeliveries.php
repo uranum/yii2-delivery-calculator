26a92ffffe4e132cbade90e3ae623e7a4133c32a
Eugeny_Emelyanov <e9139905539@gmail.com>
2018-08-27 22:07:21 +0300

    #7 fix migration for postgresql

--- migration/m170323_074600_TableDeliveries.php
+++ migration/m170323_074600_TableDeliveries.php
@@ -11,20 +11,23 @@ class m170323_074600_TableDeliveries extends Migration
 	public function up()
 	{
 		$tableOptions = null;
+        $dateExpression = $this->dateTime()->defaultExpression('CURRENT_TIMESTAMP()')->append('on update CURRENT_TIMESTAMP()');
 		if ($this->db->driverName === 'mysql') {
 			$tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
-		}
+		} elseif ($this->db->driverName === 'pgsql') {
+            $dateExpression = $this->dateTime()->defaultExpression('NOW()');
+        }
 		
 		$this->createTable($this->tableName, [
 			'id'              => $this->primaryKey(),
 			'name'            => $this->string(100)->notNull()->comment('Название'),
 			'code'            => $this->string(100)->notNull()->comment('Кодовое обозначение'),
-			'isActive'        => $this->boolean()->notNull()->defaultValue(1)->comment('Активно'),
+			'isActive'        => $this->boolean()->notNull()->defaultValue(true)->comment('Активно'),
 			'fixedCost'       => $this->integer()->comment('Фиксированная стоимость'),
 			'info'            => $this->string()->comment('Пояснительная информация'),
 			'terms'           => $this->string()->comment('Сроки доставки'),
 			'serviceShownFor' => $this->integer(10)->notNull()->comment('Доставка показывается для (регионов, города отправки, везде)'),
-			'dateDb'          => $this->dateTime()->defaultExpression('CURRENT_TIMESTAMP()')->append('on update CURRENT_TIMESTAMP()'),
+			'dateDb'          => $dateExpression,
 			'created_at'      => $this->integer()->notNull(),
 			'updated_at'      => $this->integer()->notNull(),
 		], $tableOptions);
